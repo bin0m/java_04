d7abed1c26fcf4b8acf7425e721608576074b260
Denis Stepulenok <super.denis@gmail.com>
2016-06-10 23:11:25 +0300

    Save

--- 01_Assert/src/main/java/AssertDemo.java
+++ 01_Assert/src/main/java/AssertDemo.java
@@ -2,6 +2,10 @@ import java.util.Random;
 
 // Утверждения
 // -----------
+// Книга: Программист-прагматик. Путь от подмастерья к мастеру
+// http://www.ozon.ru/context/detail/id/1657382/
+// "Мёртвые программы не лгут"
+// Подсказка 32: Пусть аварийное завершение работы программы произойдет как можно раньше
 public class AssertDemo {
 
     public static void main(String[] args) {
@@ -9,10 +13,15 @@ public class AssertDemo {
         int a = random.nextInt(10);
         System.out.println("a = " + a);
         // Проверка утверждения
+        // assert Утверждение;
+        // assert Утверждение : Сообщение_об_ошибке;
         //-->
         assert a > 10 : "a = " + a;
+        assert a > 1; // Вариант без сообщения об ошибке
         //<--
-        // Должно быть сообщение: Exception in thread "main" java.lang.AssertionError: a = 9
-        // Для включения нужен ключ VM: -ea
+        // Должно быть сообщение:
+        // Exception in thread "main" java.lang.AssertionError:
+        // a = 9
+        // Для включения нужен ключ VM: -ea (-enableAssertions)
     }
 }
